14ed091a377e804051389fe434af8ff17357df6f
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-03-30 22:58:50 -0300

    add - soma

---
+++ scripts/soma.js
@@ -0,0 +1,6 @@
+function somar() {
+    let x = parseFloat(prompt("Digite o primeiro número:"));
+    let y = parseFloat(prompt("Digite o segundo número:"));
+
+    console.log("Soma: " + (x + y));
+}
