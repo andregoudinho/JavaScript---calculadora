bb955613bdbf9bed127b9c399f6b3786a40807b3
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-03-30 22:59:03 -0300

    add - subtração

---
+++ scripts/subtracao.js
@@ -0,0 +1,6 @@
+function subtrair() {
+    let x = parseFloat(prompt("Digite o primeiro número:"));
+    let y = parseFloat(prompt("Digite o segundo número:"));
+
+    console.log("Subtração: " + (x - y));
+}
