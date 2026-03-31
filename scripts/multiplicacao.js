5451557ebc19f5a716638b013a4101e72bf7cba9
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-03-30 22:59:22 -0300

    add - multiplicação

---
+++ scripts/multiplicacao.js
@@ -0,0 +1,6 @@
+function multiplicar() {
+    let x = parseFloat(prompt("Digite o primeiro número:"));
+    let y = parseFloat(prompt("Digite o segundo número:"));
+
+    console.log("Multiplicação: " + (x * y));
+}
